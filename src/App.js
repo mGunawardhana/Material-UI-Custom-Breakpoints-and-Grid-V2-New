d4f8e52752d270e715d7cb972b06710f5e9a0407
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-07-31 11:29:29 +0530

    initial state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import {styled} from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import {Box} from "@mui/material";
+
+
+const Item = styled(Paper)(({theme}) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#28c1d4', ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<Box sx={{flexGrow: 1}}>
+            <Grid container spacing={2}>
+                <Grid item xl={12} lg={8} md={3} sm={4} xs={12}>
+                    <Item>xl=12 lg=8 md=3 sm=4 xs=12</Item>
+                </Grid>
+                <Grid item xl={12} lg={4} md={3} sm={8} xs={12}>
+                    <Item>xl=12 lg=8 md=3 sm=4 xs=12</Item>
+                </Grid>
+                <Grid item xl={12} lg={4} md={3} sm={8} xs={12}>
+                    <Item>xl=12 lg=8 md=3 sm=4 xs=12</Item>
+                </Grid>
+                <Grid item xl={12} lg={8} md={3} sm={4} xs={12}>
+                    <Item>xl=12 lg=8 md=3 sm=4 xs=12</Item>
+                </Grid>
+            </Grid>
+        </Box>);
 }
 
 export default App;
